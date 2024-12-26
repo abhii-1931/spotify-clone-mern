4567ccecef02a06e2993130f9338fe83f2048b62
abhishek choudhary <102737171+abhii-1931@users.noreply.github.com>
2024-12-27 02:29:39 +0530

    Update App.jsx
    changing the backend url from localhost to main deplyee url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 
 
-export const url = 'http://localhost:4000'
+export const url = 'https://spotify-clone-o653.onrender.com'
 
 const App = () => {
   return (
